22caed649c57284e781eb30f507827771ae21452
haakensonb <untamedhawk44@gmail.com>
2017-07-08 17:57:51 -0400

    Template folders created, initial models added

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class Author(models.Model):
+    name = models.CharField(max_length=50)
+
+class Tag(models.Model):
+    tags = models.CharField(max_length=200)
+
+class Post(models.Model):
+    title = models.CharField(max_length=50) 
+    body = models.TextField()
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    tags = models.ManyToManyField(Tag)
+    pub_date = models.DateTimeField('date published')
