77a7b5aaccd55936517a7c981e13f53922e73ee2
haakensonb <untamedhawk44@gmail.com>
2017-07-09 19:13:55 -0400

    Image uploading now working in markdownx

--- portfolio_django/urls.py
+++ portfolio_django/urls.py
@@ -16,9 +16,12 @@ Including another URLconf
 from django.conf.urls import url, include
 from django.contrib import admin
 
+from django.conf import settings
+from django.conf.urls.static import static
+
 urlpatterns = [
     url(r'^', include('portfolio.urls')),
     url(r'^blog/', include('blog.urls')),
     url(r'^markdownx/', include('markdownx.urls')),
     url(r'^admin/', admin.site.urls),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
