1fa5ede11c02712ec33e3e3463530fcb2aeb9a3c
haakensonb <untamedhawk44@gmail.com>
2017-07-09 21:42:19 -0400

    Static files moved, blog templates started, detail view created

--- blog/urls.py
+++ blog/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 app_name = 'blog'
 urlpatterns = [
-    url(r'^$', views.index, name='index')
+    url(r'^$', views.index, name='index'),
+    url(r'^(?P<post_id>[0-9]+)/$', views.detail, name='detail'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 from .models import Post
+
 
 def index(request):
     """
@@ -9,3 +10,12 @@ def index(request):
     latest_posts_list = Post.objects.order_by('-pub_date')
     context = {'latest_posts_list': latest_posts_list}
     return render(request, 'blog/index.html', context)
+
+
+def detail(request, post_id):
+    """
+    Render an individual post
+    """
+    post = get_object_or_404(Post, pk=post_id)
+    context = {'post': post}
+    return render(request, 'blog/detail.html', context)
