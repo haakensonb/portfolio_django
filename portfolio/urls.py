e64f7d743644fc42c2a376cb65f020f398ba6e1a
haakensonb <untamedhawk44@gmail.com>
2017-07-11 01:02:26 -0400

    Contact form works just needs better styling and an email driver

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -5,5 +5,7 @@ from . import views
 app_name = 'portfolio'
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'^portfolio/', views.portfolio, name='portfolio')
+    url(r'^portfolio/$', views.portfolio, name='portfolio'),
+    url(r'^contact/$', views.contact, name='contact'),
+    url(r'^success/$', views.success, name='success')
 ]
--- portfolio/views.py
+++ portfolio/views.py
@@ -1,4 +1,8 @@
-from django.shortcuts import render
+from django.core.mail import send_mail, BadHeaderError
+from django.shortcuts import render, redirect
+from django.http import HttpResponseRedirect, HttpResponse
+
+from .forms import ContactForm
 
 
 def index(request):
@@ -6,3 +10,23 @@ def index(request):
 
 def portfolio(request):
     return render(request, 'portfolio/portfolio.html')
+
+def contact(request):
+    if request.method == 'GET':
+        form = ContactForm()
+    else:
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            name = form.cleaned_data['name']
+            email = form.cleaned_data['email']
+            message = form.cleaned_data['message']
+            try:
+                send_mail('New Submission', message, email, ['admin@example.com'])
+            except BadHeaderError:
+                return HttpResponse('Invalid header found')
+            return redirect('portfolio:success')
+
+    return render(request, 'portfolio/contact.html', {'form': form})
+
+def success(request):
+    return HttpResponse('I think it was successful')
