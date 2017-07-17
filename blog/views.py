75c1915c647e9100039eeb948e81e748bedbba32
haakensonb <untamedhawk44@gmail.com>
2017-07-17 17:13:27 -0400

    Pagination added

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 from .models import Post, Tag
 
@@ -10,7 +11,21 @@ def index(request):
     """
     # Only return posts that have been published in the past
     latest_posts_list = Post.objects.filter(pub_date__lte=timezone.now()).order_by('-pub_date')
-    context = {'latest_posts_list': latest_posts_list}
+    paginator = Paginator(latest_posts_list, 8)
+
+    page = request.GET.get('page')
+    try:
+        paginated_posts = paginator.page(page)
+    except PageNotAnInteger:
+        # If page is not an integer, deliver first page
+        paginated_posts = paginator.page(1)
+    except EmptyPage:
+        # If page is out of range (e.g. 9999), deliver last page
+        paginated_posts = paginator.page(paginator.num_pages)
+
+    # latest_posts_list is kept because the sidebar archives
+    # need all the posts, not just paginated amount
+    context = {'latest_posts_list': latest_posts_list, 'paginated_posts': paginated_posts}
     return render(request, 'blog/index.html', context)
 
 
@@ -34,8 +49,22 @@ def by_tag(request, tag_word):
     # Required for sidebar view. This doesn't seem right but as of now 
     # I don't know how to do this without repeating query in multiple views.
     latest_posts_list = Post.objects.filter(pub_date__lte=timezone.now()).order_by('-pub_date')
-    context = {'posts': posts, 'tag': tag, 'latest_posts_list': latest_posts_list}
+    
+    paginator = Paginator(posts, 8)
+
+    page = request.GET.get('page')
+    try:
+        paginated_posts = paginator.page(page)
+    except PageNotAnInteger:
+        # If page is not an integer, deliver first page
+        paginated_posts = paginator.page(1)
+    except EmptyPage:
+        # If page is out of range (e.g. 9999), deliver last page
+        paginated_posts = paginator.page(paginator.num_pages)
+
+    context = {'posts': posts, 'tag': tag, 'latest_posts_list': latest_posts_list, 'paginated_posts': paginated_posts}
     return render(request, 'blog/by_tag.html', context)
+
 
 def archives(request, year, month):
     """
@@ -43,5 +72,18 @@ def archives(request, year, month):
     """
     posts = Post.objects.filter(pub_date__year=year, pub_date__month=month)
     latest_posts_list = Post.objects.filter(pub_date__lte=timezone.now()).order_by('-pub_date')
-    context = {'posts': posts, 'year': year, 'month': month, 'latest_posts_list': latest_posts_list}
+    
+    paginator = Paginator(posts, 8)
+
+    page = request.GET.get('page')
+    try:
+        paginated_posts = paginator.page(page)
+    except PageNotAnInteger:
+        # If page is not an integer, deliver first page
+        paginated_posts = paginator.page(1)
+    except EmptyPage:
+        # If page is out of range (e.g. 9999), deliver last page
+        paginated_posts = paginator.page(paginator.num_pages)
+
+    context = {'posts': posts, 'year': year, 'month': month, 'latest_posts_list': latest_posts_list, 'paginated_posts': paginated_posts}
     return render(request, 'blog/archives.html', context)
